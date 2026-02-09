1bec167f2f0a6549c3d05883f7daadb8ca03d2d1
Allie Bastien <alliebastien11@gmail.com>
2026-02-09 15:47:05 -0500

    LOGO FIX

--- Components/Entete.jsx
+++ Components/Entete.jsx
@@ -11,7 +11,7 @@ export default function Entete() {
     <div className={styles.header}>
       <div className={styles.headerContent}>
         <div className={styles.titleContainer}>
-          <img alt="Logo" src="clarea.jpg" className={styles.Logo}/>
+          <Image alt="Logo" src="clarea.jpg" className={styles.Logo}/>
         </div>
           <div className={styles.titleContainer}>
            {false == <button className={styles.langueButton}>EN</button>}
